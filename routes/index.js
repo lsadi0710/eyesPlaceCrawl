192430dc0c7c73ea61b92f79d7aba424929e36f5
전성열 <lsadi0710@naver.com>
2024-10-11 03:39:29 +0900

    [feature] 기능 구현 완료

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 let placeModule = require('../lib/place');
 router.post('/', async function(req, res, next) {
-  let result = await placeModule.searchPlaceList(req.body.keyword, req.body.type);
-  res.status(200).json(result);
+  console.log("[CALL] - ", req.body.keyword, req.body.type)
+  try{
+    let result = await placeModule.searchPlaceList(req.body.keyword, req.body.type);
+    res.status(200).json(result);
+  }catch(e){
+    res.status(400).json({
+      errorMessage: e
+    })
+  }
 });
 
 module.exports = router;
